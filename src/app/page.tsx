66192e5b7d578994dd38e0333edd92f4c4550f78
CharlesAE <charles.ad.edwards@gmail.com>
2024-03-05 22:16:24 -0500

    README

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import { Chatbot } from "@/components/Chatbot";
+import Generate from "@/components/generate";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex  flex-col items-center  p-14">
+       
+      <h2 className="mb-4 text-5xl">CharlesGPT</h2>
+      <Chatbot/>
+    </main>
+  );
+}
