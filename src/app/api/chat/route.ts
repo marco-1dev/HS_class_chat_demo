68c0dd8d9778332c9db7e806990b872561d8f665
iababio <iba-melch@hotmail.com>
2025-10-28 13:06:20 -0400

    add the fix for the api

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,25 +1,28 @@
 import { NextRequest } from "next/server";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
-
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
-
-const generationConfig = {
-    stopSequences: ["red"],
-    maxOutputTokens: 2000,
-    temperature: 0.7,
-    topP: 0.6,
-    topK: 16,
-  };
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig});
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY as string
+});
 
 export async function POST(request: NextRequest) {
 	const {messages} = await request.json();
 	const prompt = messages[messages.length - 1].content;
-	
-  const result = await model.generateContent(prompt);
-	const responseText = result.response.text();
-	
+
+	const response = await ai.models.generateContent({
+		model: "gemini-2.5-flash",
+		contents: prompt,
+		config: {
+			stopSequences: ["red"],
+			maxOutputTokens: 2000,
+			temperature: 0.7,
+			topP: 0.6,
+			topK: 16,
+		}
+	});
+
+	const responseText = response.text;
+
 	return new Response(responseText, {
 		status: 200,
 		headers: {
